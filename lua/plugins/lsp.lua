49b8090c68219ddf6e317cedf77891f6f33a4c70
phteeve <stephen.herbert@telus.net>
2024-11-06 16:57:41 -0800

    python and lua lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
           ensure_installed = { "eslint_d", "eslint-lsp" },
         }
         require("mason-lspconfig").setup {
-          ensure_installed = { "lua_ls" },
+          ensure_installed = { "lua_ls", 'pyright' },
         }
       end
     }, -- Optional
@@ -58,6 +58,37 @@ return {
       end,
       settings = {
       }
+    })
+
+     require('lspconfig').pyright.setup({
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+
+
+    require('lspconfig').lua_ls.setup({
+      settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            globals = { 'vim' },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
     })
 
 
